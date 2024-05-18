b72a7e483e0d66d7e0013e2d929fdcdd5d6f1761
Radosav Perisic <radosavperisic00@gmail.com>
2024-05-18 10:47:32 +0200

    Update Navbar.jsx

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import sphere from '/assets/the-sphere.jpg';
+import {AiOutlineMenu} from 'react-icons/ai'
+import sphere from '../assets/the-sphere.jpg'
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center h-32 max-w-[1240px] mx-auto text-white">
+    <div className="flex justify-between items-center h-28 max-w-[1240px] px-4 mx-auto text-white">
       <div>
-        <img src={sphere}/>
         <h1 className="w-full text-3xl font-bold text-[#eed349]">
           Insight
         </h1>
@@ -13,13 +13,27 @@ const Navbar = () => {
           Sphere
         </h1>
       </div>
-      <ul>
-        <li>Home</li>
-        <li>Company</li>
-        <li>Resources</li>
-        <li>About</li>
-        <li>Contact</li>
+      <ul className="flex hidden">
+      <img className="w-20 h-20" src={sphere}/>
+        <li className="p-4 text-yellow-500 font-bold">Home</li>
+        <li className="p-4 text-yellow-500 font-bold">Company</li>
+        <li className="p-4 text-yellow-500 font-bold">Resources</li>
+        <li className="p-4 text-yellow-500 font-bold">About</li>
+        <li className="p-4 text-yellow-500 font-bold">Contact</li>
       </ul>
+      <div>
+        <AiOutlineMenu size={20}/>
+      </div>
+      <div className="fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#2e5f46]">
+      <h1 className="w-full text-3xl font-bold text-[#eed349] m-4">Insight Sphere</h1>
+        <ul className="pt-12 uppercase">
+        <li className="p-4 text-yellow-500 font-bold">Home</li>
+        <li className="p-4 text-yellow-500 font-bold">Company</li>
+        <li className="p-4 text-yellow-500 font-bold">Resources</li>
+        <li className="p-4 text-yellow-500 font-bold">About</li>
+        <li className="p-4 text-yellow-500 font-bold">Contact</li>
+        </ul>
+      </div>
     </div>
   );
 };
